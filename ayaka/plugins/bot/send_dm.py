9b5b120f047373f81366969313b82019e2dfd981
KuroXD <sufyan532011@gmail.com>
2026-09-01 20:06:52 +0530

    minor fix

--- ayaka/plugins/bot/send_dm.py
+++ ayaka/plugins/bot/send_dm.py
@@ -8,7 +8,7 @@ PM_DATA:dict = {}
 async def send_dm(c:Client, m:Message):
     if m.from_user.id != Config.ADMIN_ID:
         await c.forward_messages(chat_id=Config.ADMIN_ID, from_chat_id=m.chat.id, message_ids=[m.id])
-        key = f"{m.from_user.first_name or " "} {m.from_user.last_name or " "}".strip()
+        key = f"{m.from_user.first_name or ' '} {m.from_user.last_name or ' '}".strip()
         if key not in PM_DATA:
             PM_DATA[key] = m.from_user.id
     else:
