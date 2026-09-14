1764be65ab14d0a350b911a7f2f80c9fa9dfa870
KuroXD <sufyan532011@gmail.com>
2026-09-14 19:27:21 +0530

    ...

---
+++ ayaka/plugins/bot/parse.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InputRichMessage
+from ayaka import cmd
+from ..filters import ADMINS
+
+@Client.on_message(cmd(["parse"]) & ADMINS.message(), group=72)
+async def idk_plugin(c:Client, m:Message):
+    if len(m.text.split())<2:
+        await m.reply("Wrong Format")
+    else:
+        code = " ".join(m.text.split(" "))
+        await c.send_rich_message(
+            chat_id=m.chat.id,
+            rich_message=InputRichMessage(html=code)
+        )
